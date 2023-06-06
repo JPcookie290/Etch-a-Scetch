2d974d86b4394e432c05643232e497966d670a0c
JPcookie290 <julia.paier@yahoo.com>
2023-06-06 10:19:32 +0200

    farb wechsel

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 'use strict'
 
 const btn = document.getElementById('reset');
+const container = document.getElementById('drawpad');
 
 function gridCreate() {
-    const container = document.getElementById('drawpad');
     let gridSize = 16 * 16;
     for (let i = 0; i < gridSize; i++) {
         const element = document.createElement('div');
         container.appendChild(element);
         element.classList.add('box');
     }
-    console.log(container);
+    defineListeners();
+
+}
+function defineListeners() {
+    const box = document.getElementsByClassName('box');
+    console.log(box);
+    for (const element of box) {
+        let farbe1 = Math.floor(Math.random() * 255) 
+        let farbe2 = Math.floor(Math.random() * 255) 
+        let farbe3 = Math.floor(Math.random() * 255) 
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = `rgb(${farbe1},${farbe2},${farbe3})`;
+        })
+    }
 }
 
 gridCreate();
-
-/*container.addEventListener('mouseover', () => {
-    container.style.backgroundColor = 'green';
-});*/
