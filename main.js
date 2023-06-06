eeeb51a042dac12a5c791432727dd6d634b37733
JPcookie290 <julia.paier@yahoo.com>
2023-06-06 11:15:29 +0200

    first draft

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 const btn = document.getElementById('reset');
 const container = document.getElementById('drawpad');
 
-function gridCreate() {
-    let gridSize = 16 * 16;
+function gridCreate(num) {
+    container.style.gridTemplateColumns = 'repeat('+num+', 1fr)'; 
+    container.style.gridTemplateRows = 'repeat('+num+', 1fr)';
+    let gridSize = num * num;
     for (let i = 0; i < gridSize; i++) {
         const element = document.createElement('div');
         container.appendChild(element);
@@ -16,20 +18,30 @@ function gridCreate() {
 function defineListeners() {
     const box = document.getElementsByClassName('box');
     for (const element of box) {
-        let farbe1 = Math.floor(Math.random() * 255) 
-        let farbe2 = Math.floor(Math.random() * 255) 
-        let farbe3 = Math.floor(Math.random() * 255) 
         element.addEventListener('mouseover', () => {
+            let farbe1 = Math.floor(Math.random() * 255)
+            let farbe2 = Math.floor(Math.random() * 255)
+            let farbe3 = Math.floor(Math.random() * 255)
+            console.log('mouseover')
             element.style.backgroundColor = `rgb(${farbe1},${farbe2},${farbe3})`;
         })
     }
 }
 
-gridCreate();
+
+function clear() {
+    container.innerHTML = "";
+}
 
 btn.addEventListener('click', () => {
-    const box = document.getElementsByClassName('box');
-    for (const element of box) {
-        element.style.backgroundColor = 'rgb(255,255,255)';
+    clear();
+    let zahl = parseInt(prompt("Wie groß soll das Grid sein?"))
+    if(isNaN(zahl)){
+        zahl =  parseInt(prompt("Bitte geben Sie eine Zahl ein!"))
     }
+    if(zahl <= 50 && zahl > 0 ){
+        gridCreate(zahl);
+    }else{
+        gridCreate(50);
+    }  
 })
